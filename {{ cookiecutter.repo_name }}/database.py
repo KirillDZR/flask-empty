af34ce70c5b2bf26302f0063d6ea02db227bc405
Kirill Stramnov <kirilldzr@gmail.com>
2018-02-26 16:28:05 +0300

    Adding a wrapper of the "Model" class

--- {{ cookiecutter.repo_name }}/database.py
+++ {{ cookiecutter.repo_name }}/database.py
@@ -6,6 +6,37 @@
 from extensions import db
 
 
+class BaseModel(db.Model):
+    __abstract__ = True
+    __table_args__ = {'extend_existing': True}
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.iteritems():
+            setattr(self, attr, value)
+
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+    @property
+    def url(self):
+        raise NotImplementedError('need to define url')
+
+
 def drop_all():
     db.drop_all()
 
